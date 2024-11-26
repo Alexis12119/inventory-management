fffb5c3f69eb2b096395854b76a15cb80709fb82
Alexis12119 <corporal461@gmail.com>
2024-11-26 08:36:39 +0800

    feat: update

--- src/components/modals/Equipments/AddEquipmentModal.js
+++ src/components/modals/Equipments/AddEquipmentModal.js
@@ -2,13 +2,47 @@ import React, { useState } from "react";
 
 const AddEquipmentModal = ({ isOpen, onClose, onAdd }) => {
   const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [brand, setBrand] = useState("");
+  const [model, setModel] = useState("");
+  const [serial, setSerial] = useState("");
+  const [majorCategory, setMajorCategory] = useState("");
+  const [minorCategory, setMinorCategory] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [oum, setOum] = useState("");
+  const [unitCost, setUnitCost] = useState("");
+  const [location, setLocation] = useState("");
+  const [condition, setCondition] = useState("");
   const [daysInterval, setDaysInterval] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onAdd({ name, days_interval: daysInterval });
+    onAdd({ name,
+      description,
+      brand,
+      model,
+      serial,
+      major_category: majorCategory,
+      minor_category: minorCategory,
+      quantity,
+      oum,
+      unit_cost: unitCost,
+      location,
+      condition,
+      days_interval: daysInterval });
     onClose();
     setName("");
+    setDescription("");
+    setBrand("");
+    setModel("");
+    setSerial("");
+    setMajorCategory("");
+    setMinorCategory("");
+    setQuantity("");
+    setOum("");
+    setUnitCost("");
+    setLocation("");
+    setCondition("");
     setDaysInterval("");
   };
 
@@ -20,30 +54,106 @@ const AddEquipmentModal = ({ isOpen, onClose, onAdd }) => {
         className="absolute inset-0 bg-black opacity-50"
         onClick={onClose}
       ></div>{" "}
-      <div className="bg-white rounded shadow-lg p-8  relative">
+      <div className="bg-white rounded shadow-lg p-8  relative w-96 max-w-full">
         {" "}
-        <h2 className="text-xl font-bold mb-6">Add Equipment</h2>
+        <h2 className="text-xl font-bold mb-4">Add Equipment</h2>
         <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block mb-2">Equipment Name</label>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="border rounded w-full p-3"
-              required
-            />
-          </div>
-          <div className="mb-2">
-            <label className="block mb-2">Days Interval</label>
-            <input
-              type="number"
-              value={daysInterval}
-              onChange={(e) => setDaysInterval(e.target.value)}
-              className="border rounded w-full p-3"
-              required
-            />
-          </div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Equipment Name"
+            className="border rounded w-full p-3"
+            required
+          />
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Description"
+            className="border rounded w-full p-3"
+          />
+          <input
+            type="text"
+            value={brand}
+            onChange={(e) => setBrand(e.target.value)}
+            placeholder="Brand"
+            className="border rounded w-full p-3"
+          />
+          <input
+            type="text"
+            value={model}
+            onChange={(e) => setModel(e.target.value)}
+            placeholder="Model"
+            className="border rounded w-full p-3"
+          />
+          <input
+            type="text"
+            value={serial}
+            onChange={(e) => setSerial(e.target.value)}
+            placeholder="Serial"
+            className="border rounded w-full p-3"
+          />
+          <input
+            type="text"
+            value={majorCategory}
+            onChange={(e) => setMajorCategory(e.target.value)}
+            placeholder="Major Category"
+            className="border rounded w-full p-3"
+          />
+          <input
+            type="text"
+            value={minorCategory}
+            onChange={(e) => setMinorCategory(e.target.value)}
+            placeholder="Minor Category"
+            className="border rounded w-full p-3"
+          />
+          <input
+            type="text"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+            placeholder="Quantity"
+            className="border rounded w-full p-3"
+            required
+          />
+          <input
+            type="text"
+            value={oum}
+            onChange={(e) => setOum(e.target.value)}
+            placeholder="OuM"
+            className="border rounded w-full p-3"
+            required
+          />
+          <input
+            type="text"
+            value={unitCost}
+            onChange={(e) => setUnitCost(e.target.value)}
+            placeholder="Unit Cost"
+            className="border rounded w-full p-3"
+            required
+          />
+          <input
+            type="text"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            placeholder="Location"
+            className="border rounded w-full p-3"
+          />
+          <input
+            type="text"
+            value={condition}
+            onChange={(e) => setCondition(e.target.value)}
+            placeholder="Condition"
+            className="border rounded w-full p-3"
+          />
+          <input
+            type="text"
+            value={daysInterval}
+            onChange={(e) => setDaysInterval(e.target.value)}
+            placeholder="Days Interval"
+            className="border rounded w-full p-3"
+            required
+          />
           <div className="flex justify-between">
             <button
               type="submit"
@@ -54,7 +164,7 @@ const AddEquipmentModal = ({ isOpen, onClose, onAdd }) => {
             <button
               type="button"
               onClick={onClose}
-              className="bg-gray-300 text-black py-3 px-6 rounded"
+              className="bg-gray-300 text-black  rounded ml-4 py-2 px-4"
             >
               Cancel
             </button>
--- src/components/modals/Equipments/EditEquipmentModal.js
+++ src/components/modals/Equipments/EditEquipmentModal.js
@@ -2,17 +2,53 @@ import React, { useState, useEffect } from "react";
 
 const EditEquipmentModal = ({ isOpen, onClose, onEdit, equipment }) => {
   const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [brand, setBrand] = useState("");
+  const [model, setModel] = useState("");
+  const [serial, setSerial] = useState("");
+  const [majorCategory, setMajorCategory] = useState("");
+  const [minorCategory, setMinorCategory] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [oum, setOum] = useState("");
+  const [unitCost, setUnitCost] = useState("");
+  const [location, setLocation] = useState("");
+  const [condition, setCondition] = useState("");
   const [days_interval, setDaysInterval] = useState("");
 
   useEffect(() => {
     if (equipment) {
       setName(equipment.name || "");
+      setDescription(equipment.description || "");
+      setBrand(equipment.brand || "");
+      setModel(equipment.model || "");
+      setSerial(equipment.serial || "");
+      setMajorCategory(equipment.major_category || "");
+      setMinorCategory(equipment.minor_category || "");
+      setQuantity(equipment.quantity || "");
+      setOum(equipment.oum || "");
+      setUnitCost(equipment.unit_cost || "");
+      setLocation(equipment.location || "");
+      setCondition(equipment.condition || "");
       setDaysInterval(equipment.days_interval || "");
     }
   }, [equipment]);
 
   const handleEdit = async () => {
-    if (!name) {
+    if (
+      !name &&
+      !description &&
+      !brand &&
+      !model &&
+      !serial &&
+      !majorCategory &&
+      !minorCategory &&
+      !quantity &&
+      !oum &&
+      !unitCost &&
+      !location &&
+      !condition &&
+      !days_interval
+    ) {
       alert("Please fill out all fields.");
       return;
     }
@@ -20,6 +56,17 @@ const EditEquipmentModal = ({ isOpen, onClose, onEdit, equipment }) => {
     await onEdit({
       id: equipment.id,
       name,
+      description,
+      brand,
+      model,
+      serial,
+      major_category: majorCategory,
+      minor_category: minorCategory,
+      quantity,
+      oum,
+      unit_cost: unitCost,
+      location,
+      condition,
       days_interval,
     });
     onClose();
@@ -27,21 +74,98 @@ const EditEquipmentModal = ({ isOpen, onClose, onEdit, equipment }) => {
 
   return isOpen ? (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
-      <div className="bg-white p-4 rounded w-80 max-w-xs">
-        <h2 className="text-xl font-bold mb-4">Edit Equipment</h2>
+      <div className="bg-white p-4 rounded w-96 max-w-full">
+        <h2 className="text-xl font-bold mb-2">Edit Equipment</h2>
         <input
           type="text"
           value={name}
           onChange={(e) => setName(e.target.value)}
           placeholder="Equipment Name"
-          className="border p-2 mb-4 w-full"
+          className="border p-2 mb-2 w-full"
+        />
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="Description"
+          className="border p-2 mb-2 w-full"
+        />
+        <input
+          type="text"
+          value={brand}
+          onChange={(e) => setBrand(e.target.value)}
+          placeholder="Brand"
+          className="border p-2 mb-2 w-full"
+        />
+        <input
+          type="text"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Model"
+          className="border p-2 mb-2 w-full"
+        />
+        <input
+          type="text"
+          value={serial}
+          onChange={(e) => setSerial(e.target.value)}
+          placeholder="Serial"
+          className="border p-2 mb-2 w-full"
+        />
+        <input
+          type="text"
+          value={majorCategory}
+          onChange={(e) => setMajorCategory(e.target.value)}
+          placeholder="Major Category"
+          className="border p-2 mb-2 w-full"
+        />
+        <input
+          type="text"
+          value={minorCategory}
+          onChange={(e) => setMinorCategory(e.target.value)}
+          placeholder="Minor Category"
+          className="border p-2 mb-2 w-full"
+        />
+        <input
+          type="text"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+          placeholder="Quantity"
+          className="border p-2 mb-2 w-full"
+        />
+        <input
+          type="text"
+          value={oum}
+          onChange={(e) => setOum(e.target.value)}
+          placeholder="OuM"
+          className="border p-2 mb-2 w-full"
+        />
+        <input
+          type="text"
+          value={unitCost}
+          onChange={(e) => setUnitCost(e.target.value)}
+          placeholder="Unit Cost"
+          className="border p-2 mb-2 w-full"
+        />
+        <input
+          type="text"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          placeholder="Location"
+          className="border p-2 mb-2 w-full"
+        />
+        <input
+          type="text"
+          value={condition}
+          onChange={(e) => setCondition(e.target.value)}
+          placeholder="Condition"
+          className="border p-2 mb-2 w-full"
         />
         <input
           type="text"
           value={days_interval}
           onChange={(e) => setDaysInterval(e.target.value)}
           placeholder="Days Interval"
-          className="border p-2 mb-4 w-full"
+          className="border p-2 mb-2 w-full"
         />
         <button
           onClick={handleEdit}
